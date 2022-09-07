0e87260a220f711b9a64c43bd09582831b6ece63
HARIKISHAN98 <92172801+HARIKISHAN98@users.noreply.github.com>
2022-09-07 21:53:08 +0530

    Add files via upload

---
+++ assingment 4/Q-10_print_table_of_5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int i,num=5;
+
+for ( i = 1; i <= 10; i++)
+    printf("\n%d * %d = %d ",num,i,num*i);
+
+
+    return 0;
+}
---
+++ assingment 4/Q-5_first_10_Odd_natural_number_reverse_order.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int i;
+
+for ( i = 10; i > 0; i--)
+{
+    printf("%d ",i*2-1);
+}
+
+
+    return 0;
+}
